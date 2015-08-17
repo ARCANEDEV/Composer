fe07aaa487f04579b6350c2bc402a90e791f8850
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-08-17 13:53:36 +0100

    Test the code out of him

--- tests/ComposerPluginTest.php
+++ tests/ComposerPluginTest.php
@@ -47,8 +47,8 @@ class ComposerPluginTest extends TestCase
     {
         parent::setUp();
 
-        $this->composer = $this->prophesize('Composer\Composer');
-        $this->io       = $this->prophesize('Composer\IO\IOInterface');
+        $this->composer = $this->prophesize('Composer\\Composer');
+        $this->io       = $this->prophesize('Composer\\IO\\IOInterface');
         $this->plugin   = new ComposerPlugin;
 
         $this->plugin->activate(
@@ -60,6 +60,8 @@ class ComposerPluginTest extends TestCase
     public function tearDown()
     {
         parent::tearDown();
+
+        unset($this->plugin);
     }
 
     /* ------------------------------------------------------------------------------------------------
@@ -80,7 +82,7 @@ class ComposerPluginTest extends TestCase
             ScriptEvents::POST_UPDATE_CMD,
         ];
 
-        $this->assertEquals(7, count($subscriptions));
+        $this->assertEquals(count($events), count($subscriptions));
 
 
         foreach ($events as $event) {
@@ -113,6 +115,43 @@ class ComposerPluginTest extends TestCase
 
         $root->getDevRequires()->shouldNotBeCalled();
         $root->getRepositories()->shouldNotBeCalled();
+
+        $extraInstalls = $this->triggerPlugin($root->reveal(), $dir);
+
+        $this->assertEquals(0, count($extraInstalls));
+    }
+
+    /**
+     * @test
+     *
+     * Given a root package with requires
+     *   and a composer.local.json with requires
+     *   and the same package is listed in multiple files
+     * When the plugin is run
+     * Then the root package should inherit the non-conflicting requires
+     *   and conflicting requires should be resolved 'last defined wins'.
+     */
+    public function it_can_merge_with_replace()
+    {
+        $that = $this;
+        $dir  = $this->fixtureDir('merge-with-replace');
+        $root = $this->rootFromJson("{$dir}/composer.json");
+
+        $root->setRequires(Argument::type('array'))->will(
+            function ($args) use ($that) {
+                $requires = $args[0];
+                $that->assertEquals(2, count($requires));
+                $that->assertArrayHasKey('monolog/monolog', $requires);
+                $that->assertEquals(
+                    '1.10.0',
+                    $requires['monolog/monolog']->getPrettyConstraint()
+                );
+            }
+        );
+
+        $root->getDevRequires()->shouldNotBeCalled();
+        $root->getRepositories()->shouldNotBeCalled();
+        $root->getSuggests()->shouldNotBeCalled();
 
         $extraInstalls = $this->triggerPlugin($root->reveal(), $dir);
 
@@ -165,7 +204,7 @@ class ComposerPluginTest extends TestCase
         $this->assertEquals(0, count($extraInstalls));
     }
 
-    /** @ test */
+    /** @test */
     public function it_can_one_merge_with_conflicts()
     {
         $that   = $this;
@@ -177,7 +216,7 @@ class ComposerPluginTest extends TestCase
                 $requires = $args[0];
                 $that->assertEquals(2, count($requires));
                 $that->assertArrayHasKey(
-                    'wikimedia/composer-merge-plugin',
+                    'arcanedev/composer',
                     $requires
                 );
                 $that->assertArrayHasKey('monolog/monolog', $requires);
@@ -349,17 +388,144 @@ class ComposerPluginTest extends TestCase
 
     /**
      * @test
-     * @dataProvider provideOnPostPackageInstall
+     *
+     * Given a root package with an extra section
+     *   and a composer.local.json with an extra section with no conflicting keys
+     * When the plugin is run
+     * Then the root package extra section should be extended with content from the local config.
      */
-    public function it_can_merge_on_post_package_install($package, $first)
+    public function it_can_merge_extra()
+    {
+        $that = $this;
+        $dir  = $this->fixtureDir('merge-extra');
+        $root = $this->rootFromJson("{$dir}/composer.json");
+
+        $root->setExtra(Argument::type('array'))->will(
+            function ($args) use ($that) {
+                $extra = $args[0];
+                $that->assertEquals(2, count($extra));
+                $that->assertArrayHasKey('merge-plugin', $extra);
+                $that->assertEquals(2, count($extra['merge-plugin']));
+                $that->assertArrayHasKey('foo', $extra);
+            }
+        )->shouldBeCalled();
+
+        $root->getRequires()->shouldNotBeCalled();
+        $root->getDevRequires()->shouldNotBeCalled();
+        $root->getRepositories()->shouldNotBeCalled();
+        $root->getSuggests()->shouldNotBeCalled();
+
+        $extraInstalls = $this->triggerPlugin($root->reveal(), $dir);
+
+        $this->assertEquals(0, count($extraInstalls));
+    }
+
+    /**
+     * @test
+     *
+     * Given a root package with an extra section
+     *   and a composer.local.json with an extra section with a conflicting key
+     * When the plugin is run
+     * Then the version in the root package should win.
+     */
+    public function it_can_merge_extra_with_conflict()
+    {
+        $that = $this;
+        $dir  = $this->fixtureDir('merge-extra-with-conflict');
+        $root = $this->rootFromJson("{$dir}/composer.json");
+
+        $root->setExtra(Argument::type('array'))->will(
+            function ($args) use ($that) {
+                $extra = $args[0];
+                $that->assertEquals(2, count($extra));
+                $that->assertArrayHasKey('merge-plugin', $extra);
+                $that->assertArrayHasKey('foo', $extra);
+                $that->assertEquals('bar', $extra['foo']);
+            }
+        )->shouldBeCalled();
+
+        $root->getRequires()->shouldNotBeCalled();
+        $root->getDevRequires()->shouldNotBeCalled();
+        $root->getRepositories()->shouldNotBeCalled();
+        $root->getSuggests()->shouldNotBeCalled();
+
+        $extraInstalls = $this->triggerPlugin($root->reveal(), $dir);
+
+        $this->assertEquals(0, count($extraInstalls));
+    }
+
+    /**
+     * @test
+     *
+     * Given a root package with an extra section
+     *   and replace mode is active
+     *   and a composer.local.json with an extra section with a conflicting key
+     * When the plugin is run
+     * Then the version in the composer.local.json package should win.
+     */
+    public function it_can_merge_extra_conflict_replace()
+    {
+        $that = $this;
+        $dir  = $this->fixtureDir('merge-extra-conflict-replace');
+        $root = $this->rootFromJson("{$dir}/composer.json");
+
+        $root->setExtra(Argument::type('array'))->will(
+            function ($args) use ($that) {
+                $extra = $args[0];
+                $that->assertEquals(2, count($extra));
+                $that->assertArrayHasKey('merge-plugin', $extra);
+                $that->assertArrayHasKey('foo', $extra);
+                $that->assertEquals('baz', $extra['foo']);
+            }
+        )->shouldBeCalled();
+
+        $root->getRequires()->shouldNotBeCalled();
+        $root->getDevRequires()->shouldNotBeCalled();
+        $root->getRepositories()->shouldNotBeCalled();
+        $root->getSuggests()->shouldNotBeCalled();
+
+        $extraInstalls = $this->triggerPlugin($root->reveal(), $dir);
+
+        $this->assertEquals(0, count($extraInstalls));
+    }
+
+    /**
+     * @test
+     * @dataProvider provideOnPostPackageInstall
+     *
+     * @param string $package  Package installed
+     * @param bool   $first    Expected isFirstInstall() value
+     * @param bool   $locked   Expected wasLocked() value
+     */
+    public function it_can_merge_on_post_package_install($package, $first, $locked)
     {
         $operation = new InstallOperation(
             new Package($package, '1.2.3.4', '1.2.3')
         );
         $event = $this->prophesize('Composer\\Installer\\PackageEvent');
-        $event->getOperation()->willReturn($operation)->shouldBeCalled();
+        $event->getOperation()
+            ->willReturn($operation)
+            ->shouldBeCalled();
+
+        if ($first) {
+            $locker = $this->prophesize('Composer\\Package\\Locker');
+            $locker->isLocked()
+                ->willReturn($locked)
+                ->shouldBeCalled();
+
+            $this->composer->getLocker()
+                ->willReturn($locker->reveal())
+                ->shouldBeCalled();
+
+            $event->getComposer()
+                ->willReturn($this->composer->reveal())
+                ->shouldBeCalled();
+        }
+
         $this->plugin->onPostPackageInstall($event->reveal());
-        $this->assertEquals($first, $this->plugin->isFirstInstall());
+
+        $this->assertEquals($first,  $this->plugin->isFirstInstall());
+        $this->assertEquals($locked, $this->plugin->wasLocked());
     }
 
     /**
@@ -415,6 +581,45 @@ class ComposerPluginTest extends TestCase
         $this->assertEquals($root, $getRootPackage->invoke($this->plugin));
     }
 
+    /**
+     * @test
+     *
+     * Given a root package with requires
+     *   and a b.json with requires
+     *   and an a.json with requires
+     *   and a glob of json files with requires
+     * When the plugin is run
+     * Then the root package should inherit the requires
+     *   in the correct order based on inclusion order
+     *   for individual files and alpha-numeric sorting
+     *   for files included via a glob.
+     */
+    public function it_can_correct_merge_order_of_specified_files_and_glob_files()
+    {
+        $that = $this;
+        $dir  = $this->fixtureDir('correct-merge-order-of-specified-files-and-glob-files');
+        $root = $this->rootFromJson("{$dir}/composer.json");
+
+        $expects = [
+            'merge-plugin/b.json',
+            'merge-plugin/a.json',
+            'merge-plugin/glob/a-glob2.json',
+            'merge-plugin/glob/b-glob1.json'
+        ];
+
+        $root->setRequires(Argument::type('array'))->will(
+            function ($args) use ($that, &$expects) {
+                $expectedSource = array_shift($expects);
+                $that->assertEquals(
+                    $expectedSource,
+                    $args[0]['arcanedev/workbench']->getSource()
+                );
+            }
+        );
+
+        $extraInstalls = $this->triggerPlugin($root->reveal(), $dir);
+    }
+
     /* ------------------------------------------------------------------------------------------------
      |  Other Functions
      | ------------------------------------------------------------------------------------------------
@@ -423,23 +628,25 @@ class ComposerPluginTest extends TestCase
      * Trigger the composer plugin
      *
      * @param  RootPackage $package
-     * @param  string      $directory  Working directory for composer run
+     * @param  string      $directory Working directory for composer run
+     * @param  bool        $devMode
      *
      * @return array
      */
-    protected function triggerPlugin($package, $directory)
+    protected function triggerPlugin($package, $directory, $devMode = true)
     {
         chdir($directory);
         $this->composer->getPackage()->willReturn($package);
-        $event = new Event(
+
+        $this->plugin->onInstallUpdateOrDump(new Event(
             ScriptEvents::PRE_INSTALL_CMD,
             $this->composer->reveal(),
             $this->io->reveal(),
-            true, //dev mode
+            $devMode,
             [],
             []
-        );
-        $this->plugin->onInstallUpdateOrDump($event);
+        ));
+
         $requestInstalls = [];
 
         $request = $this->prophesize('Composer\\DependencyResolver\\Request');
@@ -448,40 +655,35 @@ class ComposerPluginTest extends TestCase
                 $requestInstalls[] = $args;
             });
 
-        $event = new InstallerEvent(
+        $this->plugin->onDependencySolve(new InstallerEvent(
             InstallerEvents::PRE_DEPENDENCIES_SOLVING,
             $this->composer->reveal(),
             $this->io->reveal(),
-            true, // dev mode
-            $this->prophesize('Composer\DependencyResolver\PolicyInterface')->reveal(),
-            $this->prophesize('Composer\DependencyResolver\Pool')->reveal(),
-            $this->prophesize('Composer\Repository\CompositeRepository')->reveal(),
+            $devMode,
+            $this->prophesize('Composer\\DependencyResolver\\PolicyInterface')->reveal(),
+            $this->prophesize('Composer\\DependencyResolver\\Pool')->reveal(),
+            $this->prophesize('Composer\\Repository\\CompositeRepository')->reveal(),
             $request->reveal(),
             []
-        );
+        ));
 
-        $this->plugin->onDependencySolve($event);
-
-        $event = new Event(
+        $this->plugin->onInstallUpdateOrDump(new Event(
             ScriptEvents::PRE_AUTOLOAD_DUMP,
             $this->composer->reveal(),
             $this->io->reveal(),
-            true, //dev mode
+            $devMode,
             [],
             ['optimize' => true]
-        );
+        ));
 
-        $this->plugin->onInstallUpdateOrDump($event);
-        $event = new Event(
+        $this->plugin->onPostInstallOrUpdate(new Event(
             ScriptEvents::POST_INSTALL_CMD,
             $this->composer->reveal(),
             $this->io->reveal(),
-            true, //dev mode
+            $devMode,
             [],
             []
-        );
-
-        $this->plugin->onPostInstallOrUpdate($event);
+        ));
 
         return $requestInstalls;
     }
@@ -494,8 +696,9 @@ class ComposerPluginTest extends TestCase
     public function provideOnPostPackageInstall()
     {
         return [
-            [ComposerPlugin::PACKAGE_NAME, true],
-            ['foo/bar', false],
+            [ComposerPlugin::PACKAGE_NAME, true, true],
+            [ComposerPlugin::PACKAGE_NAME, true, false],
+            ['foo/bar', false, false],
         ];
     }
 }
